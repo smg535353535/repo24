f81e476d34c09e0c69d9e6d455a50b251bd5942b
smg535353535 <sagarmg45@gmail.com>
2024-02-03 14:49:51 +0530

    1st commit

---
+++ NewKali/src/test/java/basics/DemoTest.java
@@ -0,0 +1,14 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoTest 
+{
+	
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mvnrepository.com/");
+	}
+
+}
