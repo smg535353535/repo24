e6111e1e48f4cf3b03450031a737dc7697ca941c
smg535353535 <145663276+smg535353535@users.noreply.github.com>
2024-02-04 15:03:17 +0530

    Update DemoTest.java

--- NewKali/src/test/java/basics/DemoTest.java
+++ NewKali/src/test/java/basics/DemoTest.java
@@ -9,6 +9,7 @@ public class DemoTest
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://mvnrepository.com/");
+		System.out.println("Hi");
 		//1st pull
 	}
 
